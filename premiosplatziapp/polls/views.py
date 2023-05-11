91184a26c3b71f2e7c99db3846fd7ea771ddc18f
Andres Diaz <daletski77@gmail.com>
2023-05-10 20:50:22 -0500

    added views, models and migrations

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hi there!")
+    return HttpResponse("Hi there! Welcome to the polls area - WIP")
 # Create your views here.
--- premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatziapp/premiosplatziapp/settings.py
@@ -28,16 +28,25 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
-INSTALLED_APPS = [
+# Application definition, mejor forma de organizacion:
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+)
+
+#THIRD_APPS = (
+#    'jazzmin',
+#)
+
+LOCAL_APPS = (
+    'polls.apps.PollsConfig',
+)
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
