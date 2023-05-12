e809497fe57f74e27db8ac51ade46d9c9b4b1519
Andres Diaz <daletski77@gmail.com>
2023-05-12 11:31:35 -0500

    added important comment

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 #Creamos los modelos question y choice, recordemos que el ID viene por default gracias a Django
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("Date Published")
 
 class Choices(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) #El on_delete nos permite borrar todas las respuestas si se borra la pregunta
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
