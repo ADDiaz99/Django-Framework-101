e4d25bbe2d14c3333025a8aa15873c220f6cda99
Andres Diaz <daletski77@gmail.com>
2023-05-14 21:37:37 -0500

    added 3 new views: detail, results and vote

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hi there! Welcome to the polls area - WIP")
-# Create your views here.
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando en la pregunta numero {question_id}")
