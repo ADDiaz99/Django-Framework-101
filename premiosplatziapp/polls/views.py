97a6f3a6637076a9c5bc9758b93c294945e1643d
Andres Diaz <daletski77@gmail.com>
2023-05-17 18:57:46 -0500

    added results view and results.html

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -22,7 +22,9 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", context)
 
 def results(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render(request, "polls/results.html", context)
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
